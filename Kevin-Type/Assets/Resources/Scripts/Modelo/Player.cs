3b187794655ddab9032df31b6e11a2fd0834b8d6
FireDreigon <vleones1998@gmail.com>
2019-03-01 22:14:21 +0100

    Creacion de las clases Player, SkinPlayer y SetSkin.

---
+++ Kevin-Type/Assets/Resources/Scripts/Modelo/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+
+    public string NamePlayer { get; set; }
+    public SkinPlayer Skin;
+
+    public int HP;
+    public int Atk;
+    public int Speed;
+
+}
---
+++ Kevin-Type/Assets/Resources/Scripts/Modelo/SkinPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinPlayer
+{
+    public enum SetSkins { Mago, Vikingo, Cowboy, MaxSetSkin };
+    public enum BaseSkins { Normal, Bobo, Furioso, Mono, MaxSkin };
+
+    public BaseSkins SkinId { get; set; }
+    public Sprite BaseSkin { get; set; }
+
+    public SetSkins SombreroId { get; set; }
+    public GameObject SombreroSkin;
+
+    public SetSkins VikingoId { get; set; }
+    public GameObject VikingoSkin;
+
+    public SetSkins CowboyId { get; set; }
+    public GameObject CowboySkin;
+}
+
+[System.Serializable]
+public class SetSkin
+{
+    public string SetName { get; set; }
+    public GameObject Capa { get; set; }
+    public GameObject Sombrero { get; set; }
+    public GameObject Arma { get; set; }
+}
